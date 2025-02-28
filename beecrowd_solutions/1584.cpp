5d21fb357e47ad197d7fab4964454de7ef305509
Lucas Calumbi <lucascalumbi30@gmail.com>
2025-02-27 23:38:26 -0300

    adding beecrowd solutions folder

---
+++ beecrowd_solutions/1584.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+unsigned long long int func(int N, int K)
+{
+	
+
+
+
+
+	return R % (1000000007);
+}
+
+
+
+
+int main(void)
+{
+
+	int N, K;
+
+	while(cin >> N >> K)
+	{
+		cout << func(N,K) << endl;
+	}
+
+
+	return EXIT_SUCCESS;
+}
