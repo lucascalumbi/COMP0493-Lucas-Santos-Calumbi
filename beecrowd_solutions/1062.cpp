46702bde27dce13a5cfeccd7926e45a6875e488a
Lucas Calumbi <lucascalumbi30@gmail.com>
2025-03-02 12:48:58 -0300

    update

--- beecrowd_solutions/1062.cpp
+++ beecrowd_solutions/1062.cpp
@@ -8,40 +8,40 @@ void func(int N)
 {	
 	while(1)
 	{
-	vector<int> v(N);
-	cin >> v[0];
-	if(!v[0])
-		break;
-	for(int i = 1; i < N; i++)
-		cin >> v[i];
+		vector<int> v(N);
+		cin >> v[0];
+		if(!v[0])
+			break;
+		for(int i = 1; i < N; i++)
+			cin >> v[i];
 
-	stack<int> s;
-	bool win = true;
-	int k = 1;
-	for(int i : v)
-	{	
-		if(i == k)
-		{
-			k++;
-			continue;
-		}
-		else if(!s.empty() && i == s.top())
-			s.pop();
-		else if(i > k)
-		{
-			while(i > k)
+		stack<int> s;
+		bool win = true;
+		int k = 1;
+		for(int i : v)
+		{	
+			if(i == k)
 			{
-				s.push(k);
+				k++;
+				continue;
+			}
+			else if(!s.empty() && i == s.top())
+				s.pop();
+			else if(i > k)
+			{
+				while(i > k)
+				{
+					s.push(k);
+					k++;
+				}
 				k++;
 			}
-			k++;
+			else
+			{	
+				win = false;
+				break;
+			}
 		}
-		else
-		{	
-			win = false;
-			break;
-		}
-	}
 	if(win)
 		cout << "YES" << endl;
 	else
